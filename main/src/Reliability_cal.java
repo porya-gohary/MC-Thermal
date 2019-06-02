243ea32663a94a9958927242bd37fe71f9b3e2fa
porya-gohary <gohary.porya@gmail.com>
2019-06-02 13:43:58 +0430

    Initial Changes For Reliability Calculation

--- main/src/Reliability_cal.java
+++ main/src/Reliability_cal.java
@@ -2,14 +2,15 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.Math;
 import java.util.ArrayList;
+
+import static java.lang.Math.*;
 
 public class Reliability_cal {
     //the fault rate at the maximum voltage
-    float landa0;
+    double landa0;
     //Transient faults are usually assumed to follow a Poisson distribution with an average rate LANDA
-    float landa;
+    double landa;
     //d is a technology dependent constant
     float d;
     // scaled supply voltage
@@ -25,27 +26,42 @@ public class Reliability_cal {
     //minimum Reliability For each Task
     File Rel;
     ArrayList<Float> rel_f;
+    //Minimum Voltage
+    float v_min;
+
+    // Minimum Execution Time In Maximum Voltage And Frequency
+    float t_min;
 
     // All possible Voltage
     float [] v;
 
-    public Reliability_cal(float landa0, float landa, float d, float v_i, float v_max, float rou, float rou_min, float t_i, File rel, float [] v) {
+    //Reliability In Fault Free case
+    double R_1;
+    //Reliability In Faulty case
+    double R_2;
+
+    //Number Of Copy For Each Task
+    int n;
+
+
+    public Reliability_cal(int n,float landa0, float d,  float v_max,  float v_min, float t_i, File rel, float [] v) {
+        this.n=n;
         this.landa0 = landa0;
-        this.landa = landa;
         this.d = d;
-        this.v_i = v_i;
+
         this.v_max = v_max;
-        this.rou = rou;
-        this.rou_min = rou_min;
+        this.v_min = v_min;
+
         this.t_i = t_i;
         Rel = rel;
         this.v=v;
     }
 
     public Reliability_cal() {
+        Read_file();
     }
 
-    public void cal (){
+    public void Read_file (){
         //Read Reliability From File
         rel_f=new ArrayList<>();
         BufferedReader reader;
@@ -66,5 +82,30 @@ public class Reliability_cal {
             e.printStackTrace();
         }
     }
+
+    public void cal(){
+        rou_min=v_min/v_max;
+        for(float v_i:v){
+            t_i=t_min*v_max/v_i;
+            rou=v_i/v_max;
+            landa=(landa0*pow(10, ((d*(1-rou))/(1-rou_min))));
+            double r=exp(((-1)*landa*t_i/rou));
+            R_1=pow(r,ceil(n/2));
+            for (int l = 0; l < (floor(n/2)); l++) {
+                R_2+=combinations(n,l)*(pow((1-r),l))*(pow(r,(n-l)));
+            }
+        }
+    }
+
+    private int combinations(int n, int k){
+        return factorial(n) / (factorial (k) * factorial (n-k));
+    }
+    private int factorial(int n){
+        if (n == 0)
+            return 1;
+        else
+            return(n * factorial(n-1));
+    }
+
 
 }
--- main/src/main.java
+++ main/src/main.java
@@ -4,10 +4,13 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
 
+
 public class main {
     public static void main(String args[]) throws IOException, SAXException, ParserConfigurationException {
         Reliability_cal rc=new Reliability_cal();
-        rc.cal();
+       // rc.cal();
+
+
         File file=new File("C:\\Users\\PC Khafan\\Desktop\\MC-Thermal\\test.xml");
         dag_Reader dr=new dag_Reader(file);
         dr.readXML();
