88d94974b2b03378e03631533ff6a21941224caa
porya-gohary <gohary.porya@gmail.com>
2019-06-02 17:22:14 +0430

    Initial Changes For Reliability Calculation

--- main/src/Reliability_cal.java
+++ main/src/Reliability_cal.java
@@ -12,28 +12,28 @@ public class Reliability_cal {
     //Transient faults are usually assumed to follow a Poisson distribution with an average rate LANDA
     double landa;
     //d is a technology dependent constant
-    float d;
+    double d;
     // scaled supply voltage
-    float v_i;
+    double v_i;
     // maximum supply voltage
-    float v_max;
+    double v_max;
     //ratio of v_i / v_max
-    float rou;
+    double rou;
     //ratio of v_min / v_max
-    float rou_min;
+    double rou_min;
     //Execution Time
-    float t_i;
+    double t_i;
     //minimum Reliability For each Task
     File Rel;
     ArrayList<Float> rel_f;
     //Minimum Voltage
-    float v_min;
+    double v_min;
 
     // Minimum Execution Time In Maximum Voltage And Frequency
-    float t_min;
+    double t_min;
 
     // All possible Voltage
-    float [] v;
+    double [] v;
 
     //Reliability In Fault Free case
     double R_1;
@@ -41,10 +41,10 @@ public class Reliability_cal {
     double R_2;
 
     //Number Of Copy For Each Task
-    int n;
+    double n;
 
 
-    public Reliability_cal(int n,float landa0, float d,  float v_max,  float v_min, float t_i, File rel, float [] v) {
+    public Reliability_cal(int n,double landa0, double d,  double v_max, double v_min, double t_min, File rel, double [] v) {
         this.n=n;
         this.landa0 = landa0;
         this.d = d;
@@ -52,14 +52,15 @@ public class Reliability_cal {
         this.v_max = v_max;
         this.v_min = v_min;
 
-        this.t_i = t_i;
+        this.t_min = t_min;
         Rel = rel;
         this.v=v;
+        cal();
     }
 
-    public Reliability_cal() {
-        Read_file();
-    }
+//    public Reliability_cal() {
+//        Read_file();
+//    }
 
     public void Read_file (){
         //Read Reliability From File
@@ -85,16 +86,21 @@ public class Reliability_cal {
 
     public void cal(){
         rou_min=v_min/v_max;
-        for(float v_i:v){
-            t_i=t_min*v_max/v_i;
+        for(double v_i:v){
             rou=v_i/v_max;
+            t_i=t_min/rou;
             landa=(landa0*pow(10, ((d*(1-rou))/(1-rou_min))));
-            double r=exp(((-1)*landa*t_i/rou));
-            R_1=pow(r,ceil(n/2));
+            landa=(-1)*landa;
+            double r=exp((landa*t_i/rou));
+            R_1=pow(r,(ceil(n/2)));
             for (int l = 0; l < (floor(n/2)); l++) {
-                R_2+=combinations(n,l)*(pow((1-r),l))*(pow(r,(n-l)));
+                R_2+=combinations((int) n,l)*(pow((1-r),l))*(pow(r,(n-l)));
             }
+            System.out.println("Reliability For " + v_i + " v =  "+(R_1+R_2)+" ");
+            R_1=0;
+            R_2=0;
         }
+
     }
 
     private int combinations(int n, int k){
