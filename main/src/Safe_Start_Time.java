c0903242a1dd88b1bf3054f3d384f4f0060f5a08
porya-gohary <gohary.porya@gmail.com>
2019-06-12 16:12:38 +0430

    Add TSP Support In Reverse Scheduling

--- main/src/Safe_Start_Time.java
+++ main/src/Safe_Start_Time.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019 Porya Gohary
+ * Copyright © 2019 Porya Gohary
  * Written by Porya Gohary (Email: gohary@ce.sharif.edu)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
